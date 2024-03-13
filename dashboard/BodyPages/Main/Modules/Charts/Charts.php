6314ba8ebb7c667ef7cb8c79e6c4435fd946ecc2
Crazy4Electrons <eds4eer@outlook.com>
2024-03-13 06:46:51 +0200

    learn chrts

--- dashboard/BodyPages/Main/Modules/Charts/Charts.php
+++ dashboard/BodyPages/Main/Modules/Charts/Charts.php
@@ -1,8 +1,9 @@
 <div class="ChartTab">
-<script src="https://www.gstatic.com/charts/loader.js"></script>
-    <div id="myChart" style="width:100%; max-width:600px; height:500px;"></div>
 </div>
+<canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+<!-- <div id="myChart" style="width:100%; max-width:600px; height:500px;"></div> -->
+<!-- <script src="https://www.gstatic.com/charts/loader.js"></script> -->
 <!-- <canvas id="myChart" style="width:100%;max-width:600px"></canvas> -->
-<!-- <script src="dashboard/BodyPages/Main/Modules/Charts/jsFiles/GoogleCharts.js" async="false" crossorigin="Google"></script> -->
-<script src="dashboard/BodyPages/Main/Modules/Charts/jsFiles/Chart.js" async="false" crossorigin="anonymous"></script>
+<script src="dashboard/BodyPages/Main/Modules/Charts/jsFiles/Charts2.js" ></script>
+<script src="dashboard/BodyPages/Main/Modules/Charts/jsFiles/Chart.js" "></script>
 <link rel="stylesheet" href="dashboard/BodyPages/Main/Modules/Charts/Styles/Charts.css" media="print" onload="this.media='all'">
--- dashboard/BodyPages/Main/Modules/Charts/jsFiles/Chart.js
+++ dashboard/BodyPages/Main/Modules/Charts/jsFiles/Chart.js
@@ -1,49 +1,23 @@
-importScripts(GoogleCharts.js)
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+const yValues = [55, 49, 44, 24, 15];
+const barColors = ["red", "green","blue","orange","brown"];
 
-function drawChart() {
+new Chart("myChart") {
+  const cfg = {
+    type: 'line',
+    data: {
+      datasets: [{
+        data: [{x: 10, y: 20}, {x: 15, y: null}, {x: 20, y: 10}]
+      }]
+    }
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "World Wine Production 2018"
+    }
+  }
+});
 
-// Set Data
-const data = google.visualization.arrayToDataTable([
-  ['Contry', 'Mhl'],
-  ['Italy', 55],
-  ['France', 49],
-  ['Spain', 44],
-  ['USA', 24],
-  ['Argentina', 15]
-]);
-
-// Set Options
-const options = {
-  title: 'World Wide Wine Production'
-};
-
-// Draw
-const chart = new google.visualization.BarChart(document.getElementById('myChart'));
-chart.draw(data, options);
-
-}
-// const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-// const yValues = [55, 49, 44, 24, 15];
-// const barColors = ["red", "green","blue","orange","brown"];
-
-// new Chart("myChart", {
-//   type: "bar",
-//   data: {
-//     labels: xValues,
-//     datasets: [{
-//       backgroundColor: barColors,
-//       data: yValues
-//     }]
-//   },
-//   options: {
-//     legend: {display: false},
-//     title: {
-//       display: true,
-//       text: "World Wine Production 2018"
-//     }
-//   }
-// });
-
-window.addEventListener("load",drawChart());
+// window.addEventListener("load",drawChart());
